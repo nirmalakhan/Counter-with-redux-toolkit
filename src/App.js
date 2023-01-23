89bda905a71abfdc9ef1bdff1adcd3fd352fb2ad
nirmalakhan <nirmal3004@gmail.com>
2023-01-23 10:58:29 +0530

    Counter using Redux-toolkit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import CounterToolkit from './Components/CounterToolkit';
+
+function App() {
+  return (
+    <div className="App">
+      <CounterToolkit />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/CounterToolkit.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  increment,
+  decrement,
+  reset,
+  incrementByAmount,
+} from "./counterReduxToolkit/counterSlice";
+
+const CounterToolkit = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
+  const [incrementAmount, setIncrementAmount] = useState(0);
+
+  const addValue = Number(incrementAmount) || 0;
+
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  };
+
+  return (
+    <section>
+      <h1>{count}</h1>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+
+      <input
+        type="text"
+        value={incrementAmount}
+        onChange={(e) => setIncrementAmount(e.target.value)}
+      />
+
+      <div>
+        <button onClick={() => dispatch(incrementByAmount(addValue))}>
+          Add Amount
+        </button>
+        <button onClick={resetAll}>Reset</button>
+      </div>
+    </section>
+  );
+};
+
+export default CounterToolkit;
